8790e15a93d35ee3ed1ccb86d16a9123df51bcf5
Peter Verhas <peter@verhas.com>
2025-06-15 22:01:48 +0200

    name JavaDoc was generated

--- core/src/main/java/ch/turic/builtins/macros/Name.java
+++ core/src/main/java/ch/turic/builtins/macros/Name.java
@@ -7,7 +7,18 @@ import ch.turic.builtins.functions.FunUtils;
 import ch.turic.commands.Identifier;
 
 /**
- * returns true if the argument is defined
+ * Represents a macro implementation in the Turi language that processes the "name" function.
+ * This class implements the {@code TuriMacro} interface and provides functionality to handle
+ * execution of the "name" macro within the Turi language context.
+ * <p>
+ * The "name" macro is designed to process an argument, verify if it is an identifier, and
+ * retrieve its name based on the current execution context. If the argument is not an
+ * identifier, an {@code ExecutionException} is thrown to indicate an error.
+ * <p>
+ * Methods:
+ * - {@code name()}: Returns the name of the macro.
+ * - {@code call(Context context, Object[] arguments)}: Executes the macro with the provided
+ * context and arguments.
  */
 public class Name implements TuriMacro {
 
