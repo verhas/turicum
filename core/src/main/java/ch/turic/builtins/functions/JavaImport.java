6a909b65c822c011285e99250daf3e8d419ed6c8
Peter Verhas <peter@verhas.com>
2025-12-01 09:57:33 +0100

    java_import documentation

--- core/src/main/java/ch/turic/builtins/functions/JavaImport.java
+++ core/src/main/java/ch/turic/builtins/functions/JavaImport.java
@@ -1,6 +1,7 @@
 package ch.turic.builtins.functions;
 
 import ch.turic.Context;
+import ch.turic.SnakeNamed;
 import ch.turic.TuriFunction;
 import ch.turic.exceptions.ExecutionException;
 import ch.turic.memory.LngObject;
@@ -9,9 +10,26 @@ import ch.turic.utils.PackageLister;
 
 === `java_import`
 
+Import the classes from a Java package.
+
+The function returns a map of classes from the specified package.
+The keys are the class names, and the values are the Java classes themselves.
+
+Listing classes from the classpath is not always possible.
+The possibility depends on the actual class loaders.
+For example, importing standard JDK packages will not work.
+
+The following example imports the Turicum exception classes:
+
+{%S java_import%}
+
+As you can see, the classes are present in the object with their simple names as well as with their FQN (canonical) name.
+This redundancy is intended to avoid ambiguity that can occur when a package is deeply nested, and two different classes have the same simple name.
+In that case, only one of them will be included with the simple name, but both are present with the FQN.
+
 
 end snippet */
-
+@SnakeNamed.Name( "java_import")
 public class JavaImport implements TuriFunction {
 
     @Override
@@ -23,9 +41,14 @@ public class JavaImport implements TuriFunction {
         try {
             final var classes = new PackageLister(ctx.globalContext.classLoader).listClasses(packageName);
             final var retval = LngObject.newEmpty(ctx);
-            for (final var clazz : classes) {
-                if (!clazz.getSimpleName().isEmpty()) {
-                    retval.setField(clazz.getSimpleName(), new ch.turic.memory.JavaClass(clazz));
+            for (final var klass : classes) {
+                if (!klass.getSimpleName().isEmpty()) {
+                    final var sName = klass.getSimpleName();
+                    retval.setField(sName, new ch.turic.memory.JavaClass(klass));
+                    final var cName = klass.getCanonicalName();
+                    if (cName != null) {
+                        retval.setField(cName, new ch.turic.memory.JavaClass(klass));
+                    }
                 }
             }
             return retval;
